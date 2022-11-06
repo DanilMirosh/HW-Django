4364986ff77742b9d530afb3f9d0e9592e885221
Danil Mirosh <miroshnichenko.da.vi@mail.ru>
2022-11-06 13:24:41 +0500

    add datasets and fix

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,7 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    response = {'status': 'ok'}
+    return JsonResponse(response, status=200)
